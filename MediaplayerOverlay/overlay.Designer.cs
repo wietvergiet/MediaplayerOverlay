250c43f8861c77e3b8e4ec2ea4d296e0abef273b
Jesse <jesseooms@gmail.com>
2020-09-14 20:38:48 +0200

    First commit

---
+++ MediaplayerOverlay/overlay.Designer.cs
@@ -0,0 +1,123 @@
+﻿using System;
+
+namespace MediaplayerOverlay
+{
+    partial class MediaplayerOverlay
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.text = new System.Windows.Forms.Label();
+            this.Artist = new System.Windows.Forms.Label();
+            this.Track = new System.Windows.Forms.Label();
+            this.line = new System.Windows.Forms.Label();
+            this.refreshTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // text
+            // 
+            this.text.AutoSize = true;
+            this.text.Font = new System.Drawing.Font("Liberation Mono", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.text.ForeColor = System.Drawing.Color.Lime;
+            this.text.Location = new System.Drawing.Point(128, 14);
+            this.text.Name = "text";
+            this.text.Size = new System.Drawing.Size(82, 12);
+            this.text.TabIndex = 0;
+            this.text.Text = "Now playing";
+            // 
+            // Artist
+            // 
+            this.Artist.Font = new System.Drawing.Font("Liberation Mono", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Artist.ForeColor = System.Drawing.Color.Lime;
+            this.Artist.Location = new System.Drawing.Point(12, 50);
+            this.Artist.Name = "Artist";
+            this.Artist.Size = new System.Drawing.Size(323, 33);
+            this.Artist.TabIndex = 1;
+            this.Artist.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Track
+            // 
+            this.Track.Font = new System.Drawing.Font("Liberation Mono", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Track.ForeColor = System.Drawing.Color.Lime;
+            this.Track.Location = new System.Drawing.Point(12, 87);
+            this.Track.Name = "Track";
+            this.Track.Size = new System.Drawing.Size(323, 36);
+            this.Track.TabIndex = 2;
+            this.Track.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // line
+            // 
+            this.line.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(16)))), ((int)(((byte)(16)))));
+            this.line.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.line.Location = new System.Drawing.Point(0, 36);
+            this.line.Name = "line";
+            this.line.Size = new System.Drawing.Size(347, 1);
+            this.line.TabIndex = 3;
+            // 
+            // refreshTimer
+            // 
+            this.refreshTimer.Enabled = true;
+            this.refreshTimer.Interval = 2500;
+            this.refreshTimer.Tick += new System.EventHandler(this.refreshTimer_Tick);
+            // 
+            // MediaplayerOverlay
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(347, 136);
+            this.ControlBox = false;
+            this.Controls.Add(this.line);
+            this.Controls.Add(this.Track);
+            this.Controls.Add(this.Artist);
+            this.Controls.Add(this.text);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Location = new System.Drawing.Point(2800, 150);
+            this.Name = "MediaplayerOverlay";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "overlay";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.overlay_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+
+        #endregion
+
+        private System.Windows.Forms.Label text;
+        private System.Windows.Forms.Label Artist;
+        private System.Windows.Forms.Label Track;
+        private System.Windows.Forms.Label line;
+        private System.Windows.Forms.Timer refreshTimer;
+    }
+}
+
---
+++ MediaplayerOverlay/overlay.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MediaplayerOverlay
+{
+    public partial class MediaplayerOverlay : Form
+    {
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern UInt32 GetWindowLong(IntPtr hWnd, int nIndex);
+        [DllImport("user32.dll")]
+        static extern int SetWindowLong(IntPtr hWnd, int nIndex, IntPtr dwNewLong);
+        [DllImport("user32.dll")]
+        static extern bool SetLayeredWindowAttributes(IntPtr hwnd, uint crKey, byte bAlpha, uint dwFlags);
+        public const int GWL_EXSTYLE = -20;
+        public const int WS_EX_LAYERED = 0x80000;
+        public const int WS_EX_TRANSPARENT = 0x20;
+        public const int LWA_ALPHA = 0x2;
+        public const int LWA_COLORKEY = 0x1;
+
+        public MediaplayerOverlay()
+        {
+            InitializeComponent();
+            SetWindowLong(this.Handle, GWL_EXSTYLE,
+            (IntPtr)(GetWindowLong(this.Handle, GWL_EXSTYLE) ^ WS_EX_LAYERED ^ WS_EX_TRANSPARENT));
+            SetLayeredWindowAttributes(this.Handle, 0, 223, LWA_ALPHA);
+        }
+
+        private void overlay_Load(object sender, EventArgs e)
+        {
+            string path1 = Path.GetFullPath(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)) + "\\playingoverlay.cfg";
+           
+            if (!File.Exists(path1))
+            {
+                // Create a file to write to with basic settings on first run. 
+                string createText = "500|500";
+                File.WriteAllText(path1, createText, Encoding.UTF8);
+            }
+            else
+                if (File.Exists(path1))
+                {
+                // Read config and parse it to something usable
+                    string rawlocation = File.ReadAllText(path1);
+                    string[] tokens = rawlocation.Split('|');
+                    int[] location = Array.ConvertAll<string, int>(tokens, int.Parse);
+                // Set window location to parsed data. 
+                    this.Left = location[0];
+                    this.Top = location[1];
+                }
+
+        }
+
+
+        private void refreshTimer_Tick(object sender, EventArgs e)
+        {
+            // Getting path to now playing data. 
+            string path = Path.GetFullPath(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)) + "\\np.txt";
+            if (File.Exists(path))
+            {
+                // Read file
+                string rawplaying = File.ReadAllText(path);
+                // Split result
+                string[] delimiters = { "|" };
+                string[] playing = rawplaying.Split(delimiters, StringSplitOptions.None);
+                // Write results to labels
+                Artist.Text = playing[0];
+                Track.Text = playing[1];
+            }
+        }
+    }
+
+  
+    
+}
